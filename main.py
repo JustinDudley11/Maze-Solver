ff5f5f466792de1c207982efbfb76e1f46abda14
JustinDudley11 <justindudley11@gmail.com>
2024-02-15 14:17:48 -0500

    Added Cell class

--- main.py
+++ main.py
@@ -42,12 +42,47 @@ class Line:
             self.point1.x, self.point1.y, self.point2.x, self.point2.y , fill=fill_color, width=2
         )
         c.pack()
+        
+class Cell:
+    def __init__(self, has_left_wall, has_right_wall, has_top_wall, has_bottom_wall, x1, x2, y1, y2, win):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+        
+    def draw(self):
+        if self.has_left_wall:
+            point1 = Point(self._x1, self._y1)
+            point2 = Point(self._x1, self._y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "black")
+        if self.has_top_wall:
+            point1 = Point(self._x1, self._y1)
+            point2 = Point(self._x2, self._y1)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "black")
+        if self.has_right_wall:
+            point1 = Point(self._x2, self._y1)
+            point2 = Point(self._x2, self._y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "black")
+        if self.has_bottom_wall:
+            point1 = Point(self._x1, self._y2)
+            point2 = Point(self._x2, self._y2)
+            line = Line(point1, point2)
+            self._win.draw_line(line, "black")
+                    
 def main():
     win = Window(800, 600)
-    point1 = Point(10, 10)
-    point2 = Point(505, 505)
-    line = Line(point1, point2)
-    win.draw_line(line, "black")
+    cell1 = Cell(True, False, True, False, 10, 90, 10, 90, win)
+    cell1.draw()
+    cell2 = Cell(True, True, True, True, 100, 180, 10, 90, win)
+    cell2.draw()
     win.wait_for_close()
     
 if __name__ == "__main__":
